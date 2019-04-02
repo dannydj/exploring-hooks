a8621b3e571fd0222ab58d49980e606f14e4de29
Danny Medrano <dmedrano@devsu.com>
2019-04-02 11:03:09 -0500

    Adding empty array as second argument to useEffect Hook, important!

--- src/DataLoader.js
+++ src/DataLoader.js
@@ -3,11 +3,14 @@ import React, { useState, useEffect } from 'react';
 const DataLoader = () => {
 	const [data, setData] = useState([]);
 
+	/** useEffect serves the same purpose as componentDidMount, componentDidUpdate, and componentWillUnmount
+	 * If you call useEffect without the last empty array you would see an infinite loop. And for solving this “bug” you would need to pass an EMPTY ARRAY as a second argument to useEffect
+	 */
 	useEffect(() => {
 		fetch('https://jsonplaceholder.typicode.com/posts')
 			.then(response => response.json())
 			.then(data => setData(data))
-	});
+	}, []); // << super important array
 
 	return (
 		<div>
