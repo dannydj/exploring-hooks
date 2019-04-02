2e9382f5a38012f0b51b550bddfd1886291d6fde
Danny Medrano <dmedrano@devsu.com>
2019-04-02 10:14:38 -0500

    Using setState in ES6 classes

---
+++ src/Button.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+	state = { buttonText: 'Click me, please' }
+
+	handleClick = () => {
+		this.setState({ buttonText: 'Thanks, been clicked!' });
+	}
+
+	render() {
+		const { buttonText } = this.state;
+		return (
+			<button onClick={this.handleClick}>{buttonText}</button>
+		);
+	}
+}
+
+export default Button;
