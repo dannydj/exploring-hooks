6b8cfc1f453ba1e9d062af9d1b7935a91c01e2dd
Danny Medrano <dmedrano@devsu.com>
2019-04-02 11:40:34 -0500

    Using async/await with useEffect

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,14 +1,19 @@
 import { useState, useEffect } from 'react';
 
-/** A custom hook is a JavaScript function whose name starts with “use”, according to the React documentation */
+/** A custom hook is a JavaScript function whose name starts with “use”, according to the React documentation
+ * You cannot return a Promise from useEffect (If I wanted to use async and await inside it)
+ */
 export default function useFetch(url) {
 	const [data, setData] = useState([]);
 
-	useEffect(() => {
-		fetch(url)
-			.then(response => response.json())
-			.then(data => setData(data))
-	}, []);
+	/** To use async and await, I have to write a function before */
+	async function getData() {
+		const response = await fetch(url);
+		const data = await response.json();
+		setData(data);
+	}
+
+	useEffect(() => getData(), []);
 
 	return data;
 }
